9991cd3fac81fc7d89ba03eaaac4e3aaff28729e
Ben Loper <loperb06@gmail.com>
2019-02-04 12:57:33 -0500

    Completed Paired Programming exercises

--- 17_FileIO_Reading_in/pair-exercise/dotnet/file-io-part1-exercises-pair/Program.cs
+++ 17_FileIO_Reading_in/pair-exercise/dotnet/file-io-part1-exercises-pair/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace file_io_part1_exercises_pair
 {
@@ -6,7 +7,53 @@ namespace file_io_part1_exercises_pair
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Ask for a filesystem path for a test file
+
+            Console.Write("Please enter the path for a text file: ");
+            string filePath = Console.ReadLine();
+
+            //string filePath = @"C:\workspace\team\week-4-pair-exercises-c-team-4\17_FileIO_Reading_in\pair-exercise\dotnet\alices_adventures_in_wonderland.txt";
+
+            // Read the content of the file
+            try
+            {
+                using(StreamReader sr = new StreamReader(filePath))
+                {
+                    int wordCount = 0;
+                    int sentenceCount = 0;
+
+                    
+                    while (!sr.EndOfStream)
+                    {
+                        char currentCharacter = (char)sr.Read();
+
+                        // Determine the number of words (delimited by white space characters)
+                        if (currentCharacter.ToString() == " " || currentCharacter.ToString() == "/n")
+                        {
+                            wordCount++; 
+                        }
+
+                        // Determine the number of sentences (delimited by either a period, exclamation mark, or question mark
+                        else if (currentCharacter.ToString() == "!" || currentCharacter.ToString() == "." || currentCharacter.ToString() == "?") 
+                        {
+                            sentenceCount++;
+                        }
+                    }
+
+                    Console.WriteLine($"Word Count: {wordCount}");
+                    Console.WriteLine($"Sentence Count: {sentenceCount}");
+
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("File does not exist");
+                Console.ReadKey();
+            }
+            // Print out the results
+
+
+            Console.ReadKey();
         }
     }
 }
