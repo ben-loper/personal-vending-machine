fd44cb62506deb282602abe1dcdc6845e6846313
johnwunderle <wunderlejohn@yahoo.com>
2019-02-07 08:57:19 -0500

    main menu setup adj

--- 19_Capstone/dotnet/Capstone/Classes/VMCLI.cs
+++ 19_Capstone/dotnet/Capstone/Classes/VMCLI.cs
@@ -13,12 +13,49 @@ namespace VendingMachineCLI
 
             //(1) Display Vending Machine Items(2) Purchase(3) Exit
 
+            bool quitMenu = false;
 
-            Console.WriteLine("1) Display Vending Machine Items");
-            Console.WriteLine("2) Purchase");
-            Console.WriteLine("3) Exit");
+            while (!quitMenu)
+            {
+                Console.Clear();
+                Console.WriteLine("1) Display Vending Machine Items");
+                Console.WriteLine("2) Purchase");
+                Console.WriteLine("3) Exit");
+                Console.WriteLine("4) Sales Report");
 
-            Console.ReadKey();
+                Console.ReadKey();
+
+                int selection = 0;
+
+                if (selection == 1)
+                {
+                    //Display items
+                }
+                else if (selection == 2)
+                {
+
+                    Console.Clear();
+                    Console.WriteLine("1) Feed Money");
+                    Console.WriteLine("2) Select Product");
+                    //Display items
+                    Console.WriteLine("3) Finish Transaction");
+
+                    Console.WriteLine("Current Money Provided: (money left variable)");
+                }
+                else if (selection == 3)
+                {
+                    Console.Clear();
+                    quitMenu = true;
+                }
+                else if (selection == 4)
+                {
+                    //Sales report
+                }
+
+
+            }
+
+
         }
 
 
