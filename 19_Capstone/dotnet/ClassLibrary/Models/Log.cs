11d415d06eb2875d1c4d4c5c7b18b1340fa68978
Ben Loper <loperb06@gmail.com>
2019-02-07 08:56:36 -0500

    Add more to the log class and updated the access modifer to public for the VendingMachine, VendingMachineItem, and Log classes

--- 19_Capstone/dotnet/ClassLibrary/Models/Log.cs
+++ 19_Capstone/dotnet/ClassLibrary/Models/Log.cs
@@ -12,8 +12,10 @@ namespace VendingMachine.Models
         /// Given the full file path, reads the pipe delimited items in the file
         /// </summary>
         /// <param name="filePath"></param>
-        public static void ReadItemsFromFile(string filePath)
+        public static void ReadItemsFromFile(string filePath, VendingMachine machine)
         {
+            List<VendingMachineItem> items = new List<VendingMachineItem>();
+
             try
             {
                 using (StreamReader sr = new StreamReader(filePath))
@@ -24,6 +26,15 @@ namespace VendingMachine.Models
             catch (Exception ex)
             {
 
+            }
+        }
+        
+        private static void CreateVendingMachineItems(List<VendingMachineItem> items, VendingMachine machine)
+        {
+
+            foreach(VendingMachineItem item in items)
+            {
+                item = new VendingMachineItem();
             }
         }
             
